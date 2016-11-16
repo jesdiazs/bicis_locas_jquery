05361d76f66988c2d86f48a20e3bd337cdbea9ba
jesdiazs <jes.diazserrano@gmail.com>
2016-11-16 14:39:04 -0300

    culminacion del ejercicio de bicis_locas_2 con Jquery

--- js/main.js
+++ js/main.js
@@ -1,64 +1,58 @@
-function crearSpan(mensaje) {
-    var span = document.createElement("span");
-    var texto = document.createTextNode(mensaje);
-    span.appendChild(texto);
-    return span;
-}
 
 function validateForm(){
-    var nombre = document.getElementById('name').value;
-    var apellido = document.getElementById('lastname').value;
-    var correo = document.getElementById('name').value;
-    var password = document.getElementById('input-password').value;
-    var seleccionBici =document.querySelector('select').value;
+    var nombre = $('#name').val();
+    var apellido = $('#lastname').val();
+    var correo = $('#input-email').val();
+    var password = $('#input-password').val();
+    var seleccionBici = $('select').val();
    
     function validaName(){
         if(nombre== null || nombre.length== 0|| /[0-9]/.test(nombre)){
-            var span=crearSpan("Debe escribir el nombre ingresando solo letras");
-            document.getElementsByClassName("name-container")[0].appendChild(span);
+            var span=("<span>Debe escribir el nombre ingresando solo letras</span>");
+            $(".name-container").append(span);
 
         }else if(nombre.charAt(0)!= nombre.charAt(0).toUpperCase()){
-            var span=crearSpan("Debe comenzar la inicial del nombre en mayuscula");
-            document.getElementsByClassName("name-container")[0].appendChild(span);
+            var span=("<span>Debe comenzar la inicial del nombre en mayuscula</span>");
+            $(".name-container").append(span);
         }
     }
     validaName();
 
     function validaApellido(){
         if(apellido== null || apellido.length== 0|| /[0-9]/.test(apellido)){
-            var span=crearSpan("Debe escribir el apellido ingresando solo letras");
-            document.getElementsByClassName("lastname-container")[0].appendChild(span);
+            var span=("<span>Debe escribir el apellido ingresando solo letras</span>");
+            $(".lastname-container").append(span);
 
         }else if(apellido.charAt(0)!= apellido.charAt(0).toUpperCase()){
-            var span=crearSpan("Debe ingresar la inicial de su apellido en mayuscula");
-            document.getElementsByClassName("lastname-container")[0].appendChild(span);
+            var span= ("<span>Debe ingresar la inicial de su apellido en mayuscula</span>");
+            $(".lastname-container").append(span);
         }
     }
     validaApellido();
 
     function validaPassword(){
         if(password== null || password.length< 6|| /[0-9]/.test(password)){
-            var span=crearSpan("Debe ingresar un password de seis digitos");
-            document.getElementsByClassName("form-group")[0].appendChild(span);
+            var span= ("<span>Debe ingresar un password de seis digitos</span>");
+            $("#input-password").parent().append(span);
         }else if(password=="password" || password=="123456" || password=="098754" ){
-            var span=crearSpan("Debe ingresar un password mas seguro");
-            document.getElementById("input-password").parentNode.appendChild(span);  
+            var span= ("<span>El password no puede ser igual a password o 123456 o 098754</span>");
+            $("#input-password").parent().append(span);  
         }
     }
     validaPassword();
 
     function validaCorreo(){
         if(!(/\S+@\S+\.\S+/.test(correo))){
-            var span=crearSpan("Debe ingresar un correo valido");
-            document.getElementsByClassName("email-container")[0].appendChild(span);
+            var span= ("<span>Debe ingresar un correo valido</span>");
+            $(".email-container").append(span);
         }
     }
     validaCorreo();
 
     function validarSeleccion(){
         if(seleccionBici=="0"){
-            var span=crearSpan("Debe seleccionar una bicicleta");
-            document.getElementsByTagName("select")[0].parentNode.appendChild(span);
+            var span= ("<span>Debe seleccionar una bicicleta</span>");
+            $("select").parent().append(span);
         }
     }
     validarSeleccion();
